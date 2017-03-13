166f2e7f5e4bc3d77cb01ccf5c110b8883050a85
Alex Huang <Alex_Huang@intuit.com>
2017-03-12 19:23:17 -0700

    Add constraints and settings page

---
+++ Tipster/SettingsViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SettingsViewController.swift
+//  Tipster
+//
+//  Created by Huang, Alex on 3/12/17.
+//  Copyright © 2017 Huang, Alex. All rights reserved.
+//
+
+import Foundation
+
+class SettingsViewController: ViewController {
+    
+}
